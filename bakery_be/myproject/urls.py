c5bdbee866757db10d3efb961cbbfab48e788c9f
Zonat-2004 <1150080038@sv.hcmunre.edu.vn>
2025-05-06 20:11:01 +0700

    oke

--- bakery_be/myproject/urls.py
+++ bakery_be/myproject/urls.py
@@ -5,10 +5,19 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 
+from rest_framework_simplejwt import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('cakeAPI.urls')),
     path('', include('cakeAPI.urls')),
+    # API đăng nhập để lấy token
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # API để làm mới access token
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
